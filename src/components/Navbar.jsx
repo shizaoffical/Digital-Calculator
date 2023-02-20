7a50a047f2cab1992024c479f2155a7cad51d688
shizaoffical <110219012+shizaoffical@users.noreply.github.com>
2023-02-20 22:33:11 +0500

    hy

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,39 @@
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import logo from "../images/header-logo.png"
+import { useLocation } from 'react-router-dom';
 
 const Navbar = () => {
   const [show, setShow] = useState(false);
+  const location = useLocation();
+  const [searchText, setSearchText] = useState("");
+  const [matchResult, setMatchResult] = useState("/Love");
+  
+    const handleSearch = (event) => {
+      event.preventDefault();
+      const matches = location.pathname.match(searchText);
+      if (matches) {
+        setMatchResult(matches);
+      } else {
+        setMatchResult(null);
+      }
+    }
+  
   return (
 
     <div>
-      {show ? <div className=' nav-search-btn-div me-auto'  >
-        <div className=' container nav-search-btn d-flex w-100 align-items-center'>
-          <i class="fa-solid fa-magnifying-glass" style={{ color: "#F7941D", fontSize: "1.6rem" }}></i>
-          <input type="text" className='w-100 px-2 ' style={{ outline: "none", border: "none", backgroundColor: "none" }} />
-          <i class="fa-solid fa-xmark" onClick={() => setShow(false)}
-            style={{ border: "none", fontSize: "1.6rem", color: "white", backgroundColor: "none " }}> </i>
-        </div></div> : null}
+      {show ?
+        <div className=' nav-search-btn-div me-auto'  >
+          <form className='sumbit container nav-search-btn d-flex w-100 align-items-center ' onSubmit={handleSearch}>
+            {/* <i class="fa-solid fa-magnifying-glass" style={{ color: "#F7941D", fontSize: "1.6rem" }}></i> */}
+            <input type="text" className='w-100 px-2 ' style={{ outline: "none", border: "none", backgroundColor: "none" }}
+            value={searchText} onChange={(event) => setSearchText(event.target.value)} />
+
+            <i class="fa-solid fa-xmark pe-3" onClick={() => setShow(false)}
+              style={{ border: "none", fontSize: "1.6rem", color: "white", backgroundColor: "none " }}> </i>
+          </form>
+        </div>
+        : null}
       <nav class=" navbar navbar-expand-lg navbar-dark ">
         <div class="container">
           <Link class="navbar-brand" to="/"><img src={logo} alt="" /> Digital Calculator</Link>
@@ -28,31 +48,7 @@ const Navbar = () => {
               <li class="nav-item"><Link class="nav-link active" aria-current="page" to="/Chemistry">Chemistry</Link></li>
               <li class="nav-item"><Link class="nav-link active" aria-current="page" to="/Physics">Physics</Link></li>
               <li class="nav-item"><Link class="nav-link active" aria-current="page" to="/Math">Math</Link></li>
-              {/* <li class="nav-item"><Link class="nav-link active" aria-current="page" to="/Others">Others</Link></li> */}
-              <li class="nav-item dropdown text-light">
-          <Link class="nav-link dropdown-toggle text-light" to="/" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown
-          </Link>
-          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-          <li><Link class="dropdown-item" to="/Health">Health</Link></li>
-                  <li><Link class="dropdown-item" to="/Unit">Unit</Link></li>
-                  <li><Link class="dropdown-item" to="/Fun">Fun</Link></li>
-                  <li><Link class="dropdown-item" to="/Love">Love</Link></li>
-                  <li><Link class="dropdown-item" to="/Weather">Weather</Link></li>
-                  <li><Link class="dropdown-item" to="/Sports">Sports</Link></li>
-                  <li><Link class="dropdown-item" to="/Engineering">Engineering</Link></li>
-                  <li><Link class="dropdown-item" to="/Financial">Financial</Link></li>
-                  <li><Link class="dropdown-item" to="/Currency">Currency</Link></li>
-          </ul>
-        </li>
-              {/* <div class="dropdown">
-                <button class="dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
-                  others
-                </button>
-                <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
-                 
-                </ul>
-              </div> */}
+              <li class="nav-item"><Link class="nav-link active" aria-current="page" to="/Area">Area</Link></li>
             </ul>
             <form class="d-flex" role="search">
               <i class="fa-solid fa-magnifying-glass " onClick={() => setShow(true)} style={{ color: "#F7941D", fontSize: "1.6rem" }}></i>
@@ -61,6 +57,13 @@ const Navbar = () => {
         </div>
 
       </nav>
+      {matchResult && (
+        <div>
+          <p>Match found:</p>
+          <p>{matchResult}</p>
+          {/* Add your own implementation here */}
+        </div>
+      )}
     </div>
   )
 }
